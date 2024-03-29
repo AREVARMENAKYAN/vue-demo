bcfc70dce8bd6a134104cdfc31d7ebc6195511db
Arevik Armenakyan <arevikarmenakyan52@gmail.com>
2024-03-29 08:44:30 +0400

    Mail validation

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,43 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+export default {
+
+  data() {
+    return {
+      inputEmail: '',
+      emailValid: false,
+      checkedEmail: '',
+      isEmailChecked: false,
+    }
+  },
+
+  methods: {
+
+    validateEmail(email) {
+      const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      return emailRegex.test(email);
+    },
+
+    checkEmail() {
+      const email = prompt('Please enter your email');
+      if (email) {
+        this.checkedEmail = email;
+        this.isEmailChecked = true;
+        this.emailValid = this.validateEmail(email);
+      }
+    },
+
+    resetEmail() {
+      this.inputEmail = '';
+      this.checkedEmail = '';
+      this.emailValid = false;
+      this.isEmailChecked = false;
+    }
+
+  }
+}
 </script>
 
 <template>
@@ -13,11 +50,26 @@ import TheWelcome from './components/TheWelcome.vue'
   </header>
 
   <main>
+
+    <button @click="checkEmail">Input Email</button>
+    <button @click="resetEmail">Reset</button>
+    <div v-if="isEmailChecked">
+      <h1 :style="{ borderColor: emailValid ? 'green' : 'red' }">{{ checkedEmail }}</h1>
+      <h4 v-if="emailValid">The email is valid</h4>
+      <h4 v-else>The email is invalid</h4>
+    </div>
+
+
+
     <TheWelcome />
   </main>
 </template>
 
 <style scoped>
+h1 {
+  border: 2px solid;
+}
+
 header {
   line-height: 1.5;
 }
