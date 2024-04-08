acec11f43884726e7a483a552180a8eb379a7712
Arevik Armenakyan <arevikarmenakyan52@gmail.com>
2024-04-08 23:21:46 +0400

    Fruit list

---
+++ src/components/Product.vue
@@ -0,0 +1,36 @@
+<script>
+import Name from './Name.vue';
+import Price from './Price.vue';
+import Description from './Description.vue';
+
+export default {
+    components: {
+        Name,
+        Price,
+        Description
+    },
+
+    props: {
+        fruit: {
+            type: Object,
+            required: true
+        }
+    }
+
+}
+
+
+</script>
+
+
+<template>
+    <div>
+        {{ fruit.name }}
+        <Name :value="fruit.name" />
+        <Price :value="fruit.price" />
+        <Description :value="fruit.description" />
+    </div>
+
+
+
+</template>
