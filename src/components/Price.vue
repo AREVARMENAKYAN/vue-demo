83be59ab1972fe7c6740ea234b5e316689980711
Arevik Armenakyan <arevikarmenakyan52@gmail.com>
2024-04-09 08:48:20 +0400

    Change Currency

---
+++ src/components/Price.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props:  {
+    price:{
+        type:Number,
+        required:true
+    }
+  },
+  
+  data() {
+    return {
+      isDollar: true,
+    };
+  },
+  methods: {
+    changeCurrency() {
+      if (this.isDollar) {
+        this.$emit('update:price', this.price * 400);
+        this.isDollar = false;
+      } else {
+        this.$emit('update:price', this.price / 400);
+        this.isDollar = true;
+      }
+    },
+  },
+};
+</script>
+
+<template>
+    <div>
+        <h1>{{ price }}</h1>
+        <button @click="changeCurrency">Change Currency</button>
+      </div>
+
+</template>
